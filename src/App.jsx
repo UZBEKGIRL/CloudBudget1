a5a1bb0f9dcf3cc67493e821ecc71a0458beed9d
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-03 11:41:39 +0500

    img component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Img from './components/Img/Img'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,6 @@ function App() {
   return (
     <>
       <div>
-
       </div>
     </>
   )
