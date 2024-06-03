60ab6834248e5c981e8f7358b549a8fdcfbe2036
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-03 13:00:13 +0500

    'test'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Test from './pages/Test'
 
 function App() {
 
   return (
     <>
       <div>
+        <Test/>
       </div>
     </>
   )
