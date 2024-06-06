ab508eeb31ac49eb9c9021b8b7bc2566dcd4aa06
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-06 10:52:58 +0500

    homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,6 +5,7 @@ import TexnologiCard from "../../components/TexnologiCard";
 import TypographyFun from "../../components/Typography";
 import cn from "./style.module.scss";
 import Img from "../../components/Img/Img";
+import Card from "../../components/Card";
 
 function HomePage(props) {
     
@@ -22,6 +23,15 @@ function HomePage(props) {
                 <Img width = '445px' height='311px' radius = '15px' img='/src/assets/Video_bg.png'></Img>
             </div>
 
+            <div className={clsx(cn['third'])}>
+                <TypographyFun tag='h1' classname='header'>Возможности</TypographyFun>
+                <div className={clsx(cn['inner'])}>
+                    <Card header='Поддерживает все валюты и карты' img='/src/assets/Bg (1).png'>Мы поддерживаем все популярные валюты и полностью настраиваем, чтобы добавить</Card>
+                    <Card header='Управляйте своими расходами на ходу' img='/src/assets/Bg (2).png'>Вы можете получить доступ к своей учетной записи из любой точки мира на любом устройстве</Card>
+                    <Card header='Управление бюджетом осуществляется в облаке' img='/src/assets/Bg.png'>Наше приложение создано для того, чтобы вы могли управлять управлять своим бюджетом. </Card>
+                </div>
+            </div>
+
             <div className={clsx(cn['texnologi'])}>
                 <TypographyFun tag='h1' classname='header'>Технологии</TypographyFun>
                 <div className={clsx(cn['in'])}>
@@ -31,6 +41,27 @@ function HomePage(props) {
                     <TexnologiCard header='CSV  Отчеты' img='/src/assets/Frame 5.png' num = '04'>Скачивайте ваши отчеты в удобном формате CSV</TexnologiCard>
 
                 </div>
+            </div>
+
+            <div className={clsx(cn['five'])}>
+                <div className={clsx(cn['inner'])}>
+                    <TypographyFun tag='h1' classname='headerw'>Сосредоточьтесь на экономии денег</TypographyFun>
+                    <TypographyFun tag='p' classname='pw'>Мы помогаем вам отслеживать ваши привычки в расходах, чтобы вы могли легко определить и сократить ненужные расходы. Просто присоединяйтесь сегодня, чтобы начать!</TypographyFun>
+                    <div className={clsx(cn['in1'])}>
+                        <a href="#"><i class="fa-brands fa-app-store"></i></a>
+                        <a href="#"><i class="fa-brands fa-google-play"></i></a>
+                    </div>
+                </div>
+                <Img width = '445px' height='311px' radius = '15px' img='/src/assets/Slider.png'></Img>
+            </div>
+
+            <div className={clsx(cn['six'])}>
+                <div className={clsx(cn['inner'])}>
+                    <TypographyFun tag='h1' classname='header'>Напишите нам</TypographyFun>
+                    <TypographyFun tag='p' classname='p'>Остались вопросы или опасения? Просто заполните форму ниже, и наша служба поддержки свяжется с вами в течение 24 часов.</TypographyFun>
+                </div>
+                <form action="">
+                </form>
             </div>
         </Layout>
     )
