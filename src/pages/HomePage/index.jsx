4a763a76260e43be024ef9bd1d5686f390d9e774
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-06-06 09:32:03 +0500

    homepage chala

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,12 +4,23 @@ import Showcase from "../../components/Showcase";
 import TexnologiCard from "../../components/TexnologiCard";
 import TypographyFun from "../../components/Typography";
 import cn from "./style.module.scss";
+import Img from "../../components/Img/Img";
 
 function HomePage(props) {
     
     return (
         <Layout>
             <Showcase></Showcase>
+            <div className={clsx(cn['second'])}>
+                <div className={clsx(cn['inner'])}>
+                    <TypographyFun tag='h1' classname='header'>Простое приложение для управления бюджетом</TypographyFun>
+                    <div className={clsx(cn['in1'])}>
+                    <TypographyFun tag='p' classname='p'>Наше приложение создано, чтобы вы могли в реальном времени управлять своим бюджетом и расходами в режиме онлайн с любого устройства. Оно предоставляет подробные отчеты о доходах и расходах с графиками, чтобы вы сразу могли увидеть расходы и бюджет. Читайте ниже, чтобы узнать больше.</TypographyFun>
+                    <a href="#">Узнать подробнее</a>
+                    </div>
+                </div>
+                <Img width = '445px' height='311px' radius = '15px' img='/src/assets/Video_bg.png'></Img>
+            </div>
 
             <div className={clsx(cn['texnologi'])}>
                 <TypographyFun tag='h1' classname='header'>Технологии</TypographyFun>
